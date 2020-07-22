dcabfe5e0774141a89cfc484d03f94827bd217ac
Serena Sian Yuan <ssy248@nyu.edu>
2020-07-22 16:25:16 -0700

    Create datastoreresult_July22.py

---
+++ may2020/datastoreresult_July22.py
@@ -0,0 +1,14 @@
+result=[]
+result.append(initialcluster)
+iframe = initialframe
+currentcluster = initialcluster
+t =True
+while t:
+    nextres = findnextclusterapp(iframe, currentcluster)
+    if str(nextres) == "nan":
+        break
+    if iframe > endframe:
+        break
+    result.append(nextres)
+    iframe = iframe+1
+    currentcluster = nextres
