1bdb95db5fc5d49fe7308f3a95249a8588b3ae5e
sy2657 <60480017+sy2657@users.noreply.github.com>
2020-07-25 13:56:03 -0700

    Create correct_complete_procedure_frequencygrid.py

---
+++ may2020/correct_complete_procedure_frequencygrid.py
@@ -0,0 +1,405 @@
+
+# keep count of total number of comparisons + number of incorrect matches
+totalcomparisons = 0
+
+c1 = 1
+c2 = int(clusteridfinal)+1
+
+# new params
+totresults = []
+totclusterids=[]
+
+#diff. category of accuracy index
+missedmatching = 0
+wrongmatching =0 # matched to a different cluster
+continuedmatching = 0 # continues erroneously 
+
+
+
+#####
+initialframe = 10
+endframe = 20
+
+
+for c in range(c1, c2):
+    arrayx = []
+    arrayy=[]
+
+    initialcluster=c
+
+    listclusterids = []
+
+    listclusterids.append(initialcluster)
+
+    obnum = 0
+
+
+    totalmap={}
+
+    previndices=[]
+    currentindices=[]
+    totalmap = {}
+
+    finalarray=[]
+
+    xvalues = []
+    yvalues = []
+
+    hxvalues =[]
+    hyvalues= []
+
+    prevmap={}
+
+    mf = defaultdict(list)
+    # maximum object/cluster id in any frame
+    mx = 20
+
+    for j in range(0, mx):
+        mf[j]=0 
+
+    matchfreq= mf
+
+    for i in range(initialframe, endframe):
+        name = "file_out"
+        name = name+str(i)
+        name = name+".csv"
+        firstrow=0
+        # clear matchfreq
+        for j in range(0, mx):
+            matchfreq[j] =0
+        with open(name) as csv_file:
+            f =0 
+            # reset hxvalues , hyvalues
+            hxvalues = []
+            hyvalues=[]
+
+            currentmap= {}
+
+            csv_reader = csv.reader(csv_file, delimiter=",")
+
+            if i==initialframe:
+                for row in csv_reader:
+                    # no need for first row skip 
+                    clusterid = float(row[0])
+                    if clusterid==initialcluster:
+                        xpoint = float(row[1])
+                        ypoint = float(row[2])
+                        #print("xpt", xpoint)
+                        #print("ypt", ypoint)
+                        arrayx.append(xpoint)
+                        arrayy.append(ypoint)
+                        xr = round(xpoint)
+                        yr = round(ypoint)
+                        fromi = dinvlookupdict[(xr,yr)]
+                        h1, i1 = dhighestfreq(fromi)
+                        prevmap[i1] = 1
+                plt.scatter(arrayx, arrayy)
+                # reset ky 
+                ky = initialcluster
+                continue
+
+            for row in csv_reader:
+                clusterid = float(row[0])
+
+                if clusterid != obnum:
+                    numo1 = float(obnum)
+                    # append into dictionary of maps
+                    totalmap[numo1] = currentmap
+                    if matchfreq[numo1] > f:
+                        f = matchfreq[numo1]
+                        print("f is", f)
+                        ky = numo1
+
+                        hxvalues = xvalues
+                        hyvalues = yvalues
+                    obnum = clusterid
+
+                    currentindices=[]
+                    currentmap= {}
+
+                    xvalues =[]
+                    yvalues =[]
+                    # append first values
+                    #xvalues.append(float(row[1]))
+                    #yvalues.append(float(row[2]))
+
+                    continue
+
+                xpoint = float(row[1])
+                ypoint = float(row[2])
+                xr = round(xpoint)
+                yr = round(ypoint)
+                xvalues.append(xpoint) # save to array 
+                yvalues.append(ypoint)
+                fromi = dinvlookupdict[(xr,yr)]
+                # function to find highest freq 
+                h1, i1 = dhighestfreq(fromi)
+                # check outlier
+                freqo =0 
+                if [xr, yr] in listoutliers:
+                    # determine highest outlier count + coordinates
+                    listofoutliers = listoutliers2[fromi]
+                    for outlierpt in listofoutliers:
+                        indexo = dinvlookupdict[(outlierpt[0], outlierpt[1])]
+                        ocount = doutlier[fromi, indexo]
+                        if ocount > freqo:
+                            freqo = ocount
+                            highindo = indexo
+                # compare with dhighestfreq
+                if freqo > h1:
+                    i1 = highindo
+                # save to indices
+                currentindices.append(i1) 
+                # save to map
+                currentmap[i1] = 1
+                # check prev map
+                val = prevmap.get(fromi)
+                if val ==None:
+                    # do nothing
+                    if search2==0:
+                        print("hi")
+                else:
+                    numo = float(obnum)
+                    matchfreq[numo]= matchfreq[numo]+1
+            # check f values at end of file
+            numo2 = float(obnum)
+            if matchfreq[numo2] > f:
+                ky = numo2
+                hxvalues = xvalues
+                hyvalues = yvalues
+                totalmap[ky]= currentmap
+            finalarray.append(ky)
+            # ky is the cluster id with the highest frequency
+
+            plt.scatter(hxvalues, hyvalues)
+
+            if len(hxvalues) ==0:
+                print("0 h vals")
+            if len(hxvalues) !=0:
+                # set prevmap to the one 
+                listclusterids.append(ky) # only append if there is next match
+                prevmap = totalmap[ky]
+            # obnum
+
+    plt.show()
+	currentframe = initialframe
+	result = []
+    result.append(initialcluster)
+    
+    iframe = initialframe
+    currentcluster = initialcluster
+    t =True
+	while t:
+        nextres = findnextclusterapp(iframe, currentcluster)
+        if str(nextres) == "nan":
+            break
+        if iframe >= endframe:
+            break
+        result.append(nextres)
+        iframe = iframe+1
+        currentcluster = nextres
+		
+	# acc eval
+	len1 = len(result) #datastore result
+    len2 = len(listclusterids) # our result 
+
+    setlen = min(len1, len2)
+	od1 = {}
+    od2 = {}
+    booleanwrong=[]
+	for j1 in range(0,setlen):
+        nolongerwrong=0
+        nolongerwrongoriginal=0
+        frameno=initialframe+j1
+        od1[frameno]=[result[j1]]
+        od2[frameno]=[listclusterids[j1]]
+        if result[j1]==listclusterids[j1]:
+            nolongerwrong=1
+            nolongerwrongoriginal=1
+        if result[j1]!= listclusterids[j1]:
+            v1 = errorclusters.get(frameno)
+            if v1!=None:
+                errorarray= errorclusters[frameno]
+                for el in errorarray:
+                    if result[j1] in el:
+                        if listclusterids[j1] in el:
+                            nolongerwrong=1
+        if nolongerwrong!=1:
+            booleanwrong.append(1) # wrong
+        else:
+            booleanwrong.append(0) # not wrong
+            if nolongerwrongoriginal==0:
+                clustering_error = clustering_error+1
+    maxlen= max(len1, len2)
+    last1 = result[setlen-1]
+    last2 = listclusterids[setlen-1]
+	
+	# clustering_error incrementation
+	if len1 != len2:
+	# iterate over setlen to max len
+        for j2 in range(setlen, maxlen):
+            nolongerwrong=0
+			nolongerwrongoriginal=0
+            frameno2 = initialframe+j2
+
+            if len1 > len2:
+                # look up
+                next2 = findnextcluster(frameno2, last2)
+                
+                if next2 == result[j2]:
+                    nolongerwrong=1
+					nolongerwrongoriginal=1
+                    #booleanwrong.append(0)
+                # check error clusters
+                v2 = errorclusters.get(frameno2)
+                #print("v2", v2)
+                if v2 != None:
+                    errorarray = errorclusters[frameno2]
+                    for el in errorarray:
+                        if result[j2] in el:
+                            if next2 in el:
+                                nolongerwrong=1
+                if nolongerwrong==1:
+                    booleanwrong.append(0) # not wrong
+					if nolongerwrongoriginal==0:
+						clustering_error=clustering_error+1
+                else:
+                    booleanwrong.append(1) # wrong
+
+                last2 = next2
+                # look up 
+
+            if len2 > len1:
+                next2 = findnextclusterapp(frameno2, last2)
+                if next2 == listclusterids[j2]:
+                    nolongerwrong=1
+					nolongerwrongoriginal=1
+                v2 = errorclusters.get(frameno2)
+                if v2 != None:
+                    errorarray = errorclusters[frameno2]
+                    for el in errorarray:
+                        if listclusterids[j2] in el:
+                            if next2 in el:
+                                nolongerwrong=1
+                if nolongerwrong==1:
+                    booleanwrong.append(0)
+					if nolongerwrongoriginal==0:
+						clustering_error= clustering_error+1
+                else:
+                    booleanwrong.append(1) # wrong
+                last2 = next2
+	d1 = {}
+	d2 = {}
+	for j1 in range(0, setlen):
+        frameno = initialframe+j1
+        # check similar clusters
+        clusters1 = od1[frameno]
+        clusters2 = od2[frameno]
+
+        simclusters = errorclusters.get(frameno)
+        if simclusters != None:
+            simarray = errorclusters[frameno]
+            # check if matches
+            for el in simarray:
+                if od1[frameno] in el:
+                    clusters1.extend(el)
+                if od2[frameno] in el:
+                    clusters2.extend(el)
+        d1[frameno] = clusters1
+        d2[frameno] = clusters2
+		
+	# add empty entries for setlen to maxlen
+	maxlen= max(len1, len2)
+    # set original clusters for up to maxlen
+    for j2 in range(setlen, maxlen):
+        frameno = initialframe+j2
+        d1[frameno] = []
+        d2[frameno] = []
+        if len1>len2:
+            d1[frameno]= [result[j2]]
+        if len2>len1:
+            d2[frameno]=[listclusterids[j2]]
+
+    d1[initialframe+maxlen] = []
+    d2[initialframe+maxlen] = []
+	
+	# evaluation 3 
+	for j in range(0, maxlen):
+
+        frameno = initialframe+j 
+        print("j is", j)
+        print("frame num is", frameno)
+
+        # all clusters within current frame
+        clust1 = d1[frameno]
+        clust2 = d2[frameno]
+		
+		for c1 in clust1:
+			nc1 = findnextclusterapp(frameno,c1)
+			if str(nc1) == "nan":
+                print("nc1 is nan")
+                continue
+			nclust1 = d1[frameno+1]
+            # only append if not already there
+            if nc1 not in nclust1:
+                nclust1.append(nc1)
+            d1[frameno+1] = nclust1
+			
+		for c2 in clust2:
+            #print("frame", frameno)
+            #print("clust", c2)
+            nc2 = findnextcluaster(frameno, c2)
+            if nc2 == -1:
+                continue
+            nclust2 = d2[frameno+1]
+            if nc2 not in nclust2:
+                nclust2.append(nc2)
+            d2[frameno+1] = nclust2
+			
+		if booleanwrong[j] == 1: # look for wrong
+            # look for matching id from both arrays
+            
+            set1 = set(clust1)
+            intersect= set1.intersection(clust2)
+            if len(intersect) >0:
+                print("intersect")
+                booleanwrong[j]=0 
+                # increment clustering error
+                clustering_error = clustering_error+1
+	bindex = 1
+    firstframewrong = -1
+    for b in booleanwrong:
+        if b == 1:
+            firstframewrong = bindex
+            break
+        bindex= bindex+1
+
+    if firstframewrong == -1:
+        totalcomparisons= totalcomparisons+maxlen
+    else:
+        totalcomparisons= totalcomparisons+firstframewrong
+	contflag=0
+	missedflag=0
+	
+	if np.sum(booleanwrong)>0:
+		if firstframewrong>len1:
+            print("continued")
+            print("array1", result)
+            print("array2", listclusterids)
+            contflag= 1
+            continuedmatching= continuedmatching+1
+        if firstframewrong > len2: 
+            print("missed")
+            print("array1", result)
+            print("array2", listclusterids)
+            missedmatching = missedmatching+1
+            missedflag=1
+        if contflag==0 and missedflag==0:
+            print("wrong")
+            print("array1", result)
+            print("array2", listclusterids)
+            wrongmatching= wrongmatching+1
+        
+        if contflag==1 and missedflag==1:
+            print("BOTH CONTINUED AND MISSED")
+        
