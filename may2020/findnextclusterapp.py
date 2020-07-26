92bced7008415df859dfd45a55c1354136a4d5c1
Serena Sian Yuan <ssy248@nyu.edu>
2020-07-26 16:52:36 -0700

    Create findnextclusterapp.py

---
+++ may2020/findnextclusterapp.py
@@ -0,0 +1,26 @@
+# rename
+
+def findnextclusterapp(iframe, initialcluster): # frame and cluster
+    datastorename = 'newdatastoreMay17.csv'
+    outputmatched =0
+    with open(datastorename) as datastore_csv_file:
+        datastore_csv_reader = csv.reader(datastore_csv_file, delimiter=",")
+
+        for row in datastore_csv_reader:
+            
+            # 1 - frame 
+            framenum = float(row[0])
+
+            # 2 - cluster id
+            clusterid = float(row[1])
+
+            matched = float(row[18])  #return this matched
+
+            if framenum < iframe:
+                continue
+
+            if clusterid == initialcluster:
+                outputmatched = matched
+                return outputmatched
+            
+    return outputmatched
