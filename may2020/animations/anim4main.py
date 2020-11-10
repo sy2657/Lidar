7985bdb7673921e2743d5ec3c49bc3288cfbc305
Serena Sian Yuan <ssy248@nyu.edu>
2020-11-10 11:49:36 -0800

    Update anim4main.py

--- may2020/animations/anim4main.py
+++ may2020/animations/anim4main.py
@@ -231,7 +231,7 @@ class AnimatedScatter(object):
         coorddata2 = toty4 # toty, cdatay
         #coorddata1 = np.array([[1,2],[2,1],[3,1],[4,1],[5,1],[6,2],[7,2],[8,3],[9,1],[10,1],[11,1],[12,1],[13,2],[14,1],[15,1]])
         #coorddata2 = np.array([[1,1],[2,1],[3,2],[4,1],[5,1],[6,1],[7,1],[8,1],[9,1],[10,2],[11,3],[12,1],[13,1],[14,1],[15,2]])
-        if self.time_elapsed > 27:
+        if self.time_elapsed > 47:
             # plot frame after
             self.scat = self.ax.scatter(followarrayx, followarrayy, c = followcolor, animated= True)
             return self.scat,
